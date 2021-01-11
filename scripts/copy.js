a2495874cc4c61ab641e2f230415d78d82ec4546
lcluber <ludovic.cluber@gmail.com>
2021-01-11 21:59:14 +0100

    update workflow for license concatenation

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,6 +1,8 @@
 // const path = require('path');
 const fs      = require('fs');
 
+const mail    = 'https://github.com/DWTechs/CheckHard.js';
+const CRLF    = '\r\n';
 const rel     = './';
 const src     = `${rel}build/`;
 const dest    = `${rel}dist/`; 
@@ -29,12 +31,12 @@ const files   = [
 
 fs.mkdir(dest, { recursive: false },(err) => {
   if (err) throw err;
-  fs.readFile(`${rel}scripts/license.js`, (err, license) => {
+  fs.readFile(`${rel}LICENSE`, (err, license) => {
     if (err) throw err;
     for (let file of files) {
       fs.readFile(file.src, (err, fileContent) => {
         if (err) throw err;
-        fs.writeFile(file.dest, license+fileContent, (err) => {
+        fs.writeFile(file.dest, `/*${CRLF}${license}${CRLF}${mail}${CRLF}*/${CRLF}${CRLF}${fileContent}`, (err) => {
           if (err) throw err;
         });
       });

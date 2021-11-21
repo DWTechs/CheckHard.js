5c808e43aed1c637bbb1cff191d6531fa630ca51
lcluber <ludovic.cluber@gmail.com>
2021-11-21 13:13:43 +0100

    Fix declaration file for Typescript

--- dist/ch.d.ts
+++ dist/ch.d.ts
@@ -35,6 +35,7 @@ declare function isAscii(code: any, extended?: boolean): boolean;
 declare function isInteger(number: any, typeCheck?: boolean): boolean;
 declare function isFloat(number: any, typeCheck?: boolean): boolean;
 declare function isNumber(number: any, typeCheck?: boolean): boolean;
+declare function isValidNumber( number: any, min?: number, max?: number, typeCheck?: boolean): boolean
 declare function isString(string: any): boolean;
 declare function isHtmlElement(htmlElement: any): boolean;
 declare function isHtmlEventAttribute(htmlEventAttribute: any): boolean;
@@ -48,4 +49,50 @@ declare function isNegative(number: any, typeCheck?: boolean): boolean;
 declare function isPowerOfTwo(number: any, typeCheck?: boolean): boolean;
 declare function isEmail(email: any): boolean;
 declare function isIpAddress(ipAddress: any): boolean;
-export { isBoolean, isNil, isSymbol, isJson, isFunction, isObject, isArray, isAscii, isInteger, isFloat, isNumber, isString, isHtmlElement, isHtmlEventAttribute, isNode, isEven, isOdd, isOrigin, isPositive, isNegative, isPowerOfTwo, isRegex, isEmail, isIpAddress };
+declare function isSlug(slug: any): boolean;
+declare function isHexadecimal(string: any): boolean;
+declare function containsUpperCase(string: any): boolean;
+declare function containsLowerCase(string: any): boolean;
+declare function containsSpecialCharacter(string: any): boolean;
+declare function containsNumber(number: any): boolean;
+declare function isDate(date: any): boolean;
+declare function isValidDate(date: any, min?: Date, max?: Date): boolean;
+declare function isTimestamp(timestamp:any): boolean;
+declare function isValidTimestamp(timestamp: any, min?: number, max?: number): boolean;
+
+
+export { isBoolean, 
+         isNil,
+         isSymbol,
+         isJson,
+         isFunction,
+         isObject,
+         isArray,
+         isAscii,
+         isInteger,
+         isFloat,
+         isNumber,
+         isValidNumber,
+         isString,
+         isHtmlElement,
+         isHtmlEventAttribute,
+         isNode,
+         isEven,
+         isOdd,
+         isOrigin,
+         isPositive,
+         isNegative,
+         isPowerOfTwo,
+         isRegex,
+         isEmail,
+         isIpAddress,
+         isSlug,
+         isHexadecimal,
+         containsUpperCase,
+         containsLowerCase,
+         containsSpecialCharacter,
+         containsNumber,
+         isDate,
+         isValidDate,
+         isTimestamp,
+         isValidTimestamp };
